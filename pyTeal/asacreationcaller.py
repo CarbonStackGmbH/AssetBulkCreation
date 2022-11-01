65d2a9d2fefd988491aba2005fc71385a58129c4
Tobias Thiel <99893630+TTogle@users.noreply.github.com>
2022-11-01 14:28:51 +0100

    Update asacreationcaller.py

--- pyTeal/asacreationcaller.py
+++ pyTeal/asacreationcaller.py
@@ -1,47 +1,51 @@
-
 from pyteal import * 
+#######################################################################################
+#         THIS PYTEAL HAS NOT YET BEEN SERUCITY AUDITTED. DO NOT USE IT               #
+#         IN A PRODUCTION ENVIRONMENT.                                                #
+#                                                                                     #
+#         THIS PYTEAL IS PART OF A ASSETBULKCREATION SCRIPT FROM CARBONSTACK GmbH.    #
+#         THIS SCRIPT WILL BE UPDATED ON A REGULAR BASIS.                             #
+#                                                                                     #
+# Current Version: 2.0                                                                #
+# Last updated: 28.10.2022                                                            #
+# Maintainer: tobias.thiel@carbonstack.de                                             #
+#                                                                                     #
+#######################################################################################
 
-# This pyTEAL is only meant for educational purposes, do not use this pyteal on a production level!           #
-# The displayed contract calls another contract that creates 16 asset with one call.                          #
-# The contract calls the creation contract of the ASA 15 times. THis means if left unconfigured it will create#
-# a total of 240 ASA per call. If another amount is needed please feel free to change the loops in this caller#
-# or inside the ASA creation contract. There is a work around as in the script associated to there contracts  #
-# there is a distinct order in which the contracts are created. The script saves the application id to a txt  #
-# file called appid.txt that in the beginning is opened, read and put into the variable id thus making it     #
-# possible to automatically compile this contract as well without knowing the app id when setting everything  #
-# up. Please change the path to the associated path needed (probably where the script is executed).           #
-# To trigger the ASA creation the app call must include the argument "create_asa".                            #
-# For the parameters the regular ASA constraints found under:                                                 #
-# https://developer.algorand.org/docs/get-details/parameter_tables/                                           #
-# apply.                                                                                                      #    
-# Maintainer:                                                                                                 #
-# Tobias Thiel |CarbonStack GmbH                                                                              #
-# tobias.thiel@carbonstack.de                                                                                 #
-
-
-
-
-
+#This part of the program reads external input to the ASA-Caller contract. 
+#First of it stores the app id of the ASA-Creation contract. Erasing the
+#need to knowing the app id before hand. In addition, the variables x1 and x2 
+#originating from a calculation script handed over from the bulkcreation script.
+#These make it possible in combination with the ASA-Creation Contract and Script. 
+#To efficiently create thousands of assets in a small timeframe.
+#TODO: Make x1 and x2 be read form a single Document.
 
 
-
-#required for not hard defining the app id of the ASA-Creation Loop reads the appID from the saved output from the script
-f = open('PATH/TO/FILE/appid.txt', 'r')
+f = open('/path/to/sandbox/appid.txt', 'r')
 app = int(f.readline())
 f.close()
 id = Int(app)
+f = open('path/to/sandbox/x1.txt', 'r')
+x1 = int(f.readline())
+f.close()
+f = open('path/to/sandbox/x2.txt', 'r')
+x2 = int(f.readline())
+f.close()
+
+
+#todo do it for the Variables for the loops! 
+
 def stateful():
-#defines a scratch variable required for looping in a contract
+   #defines a scratch variable required for looping in a contract 
     i = ScratchVar()
+    #loops calls for assetcreation smart contract 
     on_call_asa_creator= Seq([ 
-#loops calls for assetcreation smart contract change this to adjust the contract calls, anything higher than 15 needs an alteration of the creation contract
-#else the call will fail due to an op-code limitations
-                For(i.store(Int(0)), i.load() < Int(15), i.store(i.load() + Int(1)))  
+                For(i.store(Int(0)), i.load() < Int(x1), i.store(i.load() + Int(1)))  
                 .Do( 
                 InnerTxnBuilder.Begin(),
                 InnerTxnBuilder.SetFields({
                 TxnField.type_enum: TxnType.ApplicationCall,
-#calls the app id read to the program before the stateful
+                #calls the app id read to the program before the stateful
                 TxnField.application_id: id,
                 TxnField.on_completion: OnComplete.NoOp,
                 TxnField.application_args: [Bytes("create_asa")]
@@ -53,29 +57,47 @@ def stateful():
                     Int(1),
     ]
             )
-#calls the creator contract a single time creating on default option another 16 ASAs  
+    #loop call for assetcreation used for the remainder after the main creation took place
     on_call_asa_rerun= Seq([ 
+                For(i.store(Int(0)), i.load() < Int(x2), i.store(i.load() + Int(1)))  
+                .Do( 
+                InnerTxnBuilder.Begin(),
+                InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.ApplicationCall,
+                 #calls the app id read to the program before the stateful
+                TxnField.application_id: id,
+                TxnField.on_completion: OnComplete.NoOp,
+                TxnField.application_args: [Bytes("rerun_asa")]
+                 }),
+                    #Submit the transaction
+                    InnerTxnBuilder.Submit(), 
+                ),
+                    Int(1),
+    ]
+            )
+    #singular call for just calling one asa creation‚
+    on_call_asa_final= Seq([ 
                 InnerTxnBuilder.Begin(),
                 InnerTxnBuilder.SetFields({
                 TxnField.type_enum: TxnType.ApplicationCall,
                 TxnField.application_id: id,
                 TxnField.on_completion: OnComplete.NoOp,
-                TxnField.application_args: [Bytes("create_asa")]
+                TxnField.application_args: [Bytes("final_asa")]
                  }),
                     #Submit the transaction
                     InnerTxnBuilder.Submit(), 
-
+                
                     Int(1),
     ]
             )
         
-        
     
-#Conditions for the modes for the asa creation
+
     program = Cond(
         [Txn.application_id() == Int(0), Int(1)],
-        [Txn.application_args[0] == Bytes("create_asa"), on_call_asa_creator]
-        [Txn.application_args[0] == Bytes("rerun_asa"), on_call_asa_rerun]
+        [Txn.application_args[0] == Bytes("create_asa"), on_call_asa_creator],
+        [Txn.application_args[0] == Bytes("rerun_asa"), on_call_asa_rerun],
+        [Txn.application_args[0] == Bytes("final_asa"), on_call_asa_final]
 
     )
 
@@ -83,5 +105,4 @@ def stateful():
 
 
 if __name__ == "__main__":
-#requires pyTeal/Teal Version 7 might need to start sandbox with the future_template.json
     print(compileTeal(stateful(), Mode.Application, version=7))
